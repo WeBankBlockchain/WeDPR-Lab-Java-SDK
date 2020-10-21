26d24b8e43b1705fafa96c9eb9010417a3c365ec
HaoXuan40404 <444649358@qq.com>
2020-10-21 19:45:30 +0800

    add crypto interface

--- solution/crypto/src/main/java/com/webank/wedpr/crypto/NativeInterface.java
+++ solution/crypto/src/main/java/com/webank/wedpr/crypto/NativeInterface.java
@@ -1,6 +1,6 @@
 // Copyright 2020 WeDPR Lab Project Authors. Licensed under Apache-2.0.
 
-package com.webank.wedpr.vcl;
+package com.webank.wedpr.crypto;
 
 import com.webank.wedpr.common.NativeUtils;
 import java.io.IOException;
@@ -14,11 +14,11 @@ public class NativeInterface {
       String osName = System.getProperty("os.name").toLowerCase();
       String libPathInJar;
       if (osName.contains("windows")) {
-        libPathInJar = "/WeDPR_dynamic_lib/ffi_java_vcl.dll";
+        libPathInJar = "/WeDPR_dynamic_lib/ffi_java_crypto.dll";
       } else if (osName.contains("linux")) {
-        libPathInJar = "/WeDPR_dynamic_lib/libffi_java_vcl.so";
+        libPathInJar = "/WeDPR_dynamic_lib/libffi_java_crypto.so";
       } else if (osName.contains("mac")) {
-        libPathInJar = "/WeDPR_dynamic_lib/libffi_java_vcl.dylib";
+        libPathInJar = "/WeDPR_dynamic_lib/libffi_java_crypto.dylib";
       } else {
         throw new IOException(String.format("Operating system %s is not supported.", osName));
       }
@@ -30,20 +30,16 @@ public class NativeInterface {
   }
 
   // JNI function section.
-  public static native VclResult makeCredit(long value);
+  public static native CryptoResult secp256k1EciesEncrypt(String pubKey, String plaintext);
 
-  public static native VclResult proveSumBalance(String c1Secret, String c2Secret, String c3Secret);
+  public static native CryptoResult secp256k1EciesDecrypt(String priKey, String ciphertext);
 
-  public static native VclResult verifySumBalance(
-      String c1Credit, String c2Credit, String c3Credit, String proof);
+  public static native CryptoResult secp256k1GenKeyPair();
 
-  public static native VclResult proveProductBalance(
-      String c1Secret, String c2Secret, String c3Secret);
+  public static native CryptoResult keccak256Hash(String message);
 
-  public static native VclResult verifyProductBalance(
-      String c1Credit, String c2Credit, String c3Credit, String proof);
+  public static native CryptoResult secp256k1Sign(String priKey, String messageHash);
 
-  public static native VclResult proveRange(String ownerSecret);
-
-  public static native VclResult verifyRange(String confidentialCredit, String proof);
+  public static native CryptoResult secp256k1Verify(
+      String pubKey, String messageHash, String signature);
 }
